0598e0c97b2fe4b3a87cd05d7c26cfa0bd9094bf
WebBastet <webbastet@gmail.com>
2024-12-04 11:56:10 -0300

    Segundo cambio, texto modificado en navegador

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express =  require("express"); //le digo que voy a requerir el módulo express, lo voy a guardar en una constacte express
+const express =  require("express"); 
 
-const app=express();//voy a crear una constante app que será una instancia de express 
+const app=express();
 
-app.get("/", (req,res)=>res.send("Hello word with express")) //acá atraves del app.get le digo que cuando pidan la ruta inicial de la aplicación quiero que recibas un request y un response y respondas con un mensaje que diga "Hello word with express"
+app.get("/", (req,res)=>res.send("<h1>Express con html!!</h1>")) 
 
-app.listen(3000);//lo ejecuto diciéndole que quiero que funcione en el puerto 3000 que es el habilitado en AWS
-console.log("Server on port 3000") //enviamos en mje por consola para ver que funcione
+app.listen(3000);
+console.log("Server on port 3000") 
